f06ae2e38c4ba538937ddcb2d15b4d46ff7fddd5
Kelvin <kelvin.meyer@smsportal.com>
2024-11-25 19:42:17 +0200

    sync from work

---
+++ after/after/ftplugin/cs.lua
@@ -0,0 +1,7 @@
+vim.g.dotnet_errors_only = vim.v["true"]
+vim.g.dotnet_show_project_file = vim.v["true"]
+vim.cmd("compiler dotnet")
+
+--let dotnet_errors_only = v:true
+--let dotnet_show_project_file = v:false
+--compiler dotnet
